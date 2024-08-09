5640767c4e3b3b85f6ee68cd8e23892178965a5b
JAYDIP <pateljaydippj699@gmail.com>
2024-08-09 11:48:50 +0530

    add log file

---
+++ QuickApp.Server/Core/Dtos/Log1/GetLogDto.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickApp.Server.Core.Dtos.Log
+{
+    public class GetLogDto
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string? UserName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
